2e15c8eaafbaffdc26a707cf4c068106807e20ef
Alfred Emmanuel <118021171+Pycomet@users.noreply.github.com>
2023-11-19 16:00:26 +0100

    fix

--- handlers/start.py
+++ handlers/start.py
@@ -14,7 +14,7 @@ def start(msg):
     # import pdb; pdb.set_trace()
 
     bot.send_photo(
-        msg.chat.id,
+        msg.from_user.id,
         photo="https://ibb.co/DLQ8yys",
         caption=emoji.emojize(
             f"""
