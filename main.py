6f7e47a48ab90d90709d328a757e37d74d42e97b
Alfred Emmanuel <118021171+Pycomet@users.noreply.github.com>
2023-11-25 19:36:42 +0100

    remove threading

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def dashboard():
     return render_template('index.html')
     
 
-@app.route("/" + TOKEN, methods=["POST", "GET"])
+@app.route("/" + TOKEN, methods=["POST"])
 def checkWebhook():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
-    )
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    print(update)
+    bot.process_new_updates([update])
     return "Your bot application is still active!", 200
 
 
